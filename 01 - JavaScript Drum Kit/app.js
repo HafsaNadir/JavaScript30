b96895d38686f6854c345e02760417d9a8ea6207
Hafsa Nadir <hafsanadir21@gmail.com>
2019-09-07 19:03:14 -0700

    javascript drum kit project

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,18 @@
+const soundPlaying = (e) =>
+{
+  console.log('abc')
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    if(!audio) return
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+function removeTransition(e){
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener("keydown",soundPlaying)
